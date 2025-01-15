f21d99f1b616f69d951323e88c4e2cb943c48706
Arpit Tiwari <tdm75513@gmail.com>
2025-01-15 17:15:36 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
+  server: {
     port: 7777,
     host: '0.0.0.0',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
   }
 })
